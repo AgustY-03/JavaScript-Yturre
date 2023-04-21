3b22cdd3079cac91569b21e2a94486db7674c16d
Gamer <AgustinYturre@hotmail.com>
2023-04-20 21:41:38 -0300

    3ra Preentregra

--- js/carro.js
+++ js/carro.js
@@ -1,30 +1,141 @@
-const RCarro = []
-
-class Productos {
-    constructor (obj){
-        this.id = obj.id;
-        this.nombre = obj.manga
-        this.precio = parseFloat(obj.precio);
-    }
-}
-
-const renderCarro = JSON.parse(localStorage.getItem('saveStateCarro'));
+let RCarro = JSON.parse(localStorage.getItem('saveStateCarro')) || [];
 
 function renderCarrito(){
+    let montoAPagar = 0;
     let body = document.getElementById('tablebody');
-    for(const render of renderCarro){
-        RCarro.push(new Productos(render));
-    } 
-    console.table(RCarro)
     for(const carro of RCarro){
         body.innerHTML += `
             <tr>
-                <td>${RCarro.id}</td>
-                <td>${RCarro.nombre}</td>
-                <td>${RCarro.precio}</td>
+                <td>${carro.id}</td>
+                <td>${carro.manga} ${carro.num}</td>
+                <td>${carro.editorial}</td>
+                <td>$${carro.precio}</td>
             </tr>
         `;
+        // Incrementar total.
+        montoAPagar = montoAPagar + carro.precio;
+        document.getElementById('total').innerText = 'Total a pagar $: '+montoAPagar;
     }
 }
 
 renderCarrito();
+
+const finalizar = document.getElementById('finalizarCompra')
+finalizar.addEventListener('click', () =>{
+    if(RCarro && RCarro.length >= 1){
+    const finalizarModal = document.getElementById('modalFinalizar');
+    finalizarModal.innerHTML = `
+    <div id="modal-container">
+        <div id="modal2" class="hidden">
+        <h3>Formulario de pago</h3>
+            <button id="close-modal2" class="btn btn-danger">Cerrar</button>
+            <div id="modal-content">
+          <form id="finalizarPagoModal">
+            <div class="form-position">
+              <div class="nombreModal form-width">
+                <label class="input_label" for="nombre">Nombre:</label>
+                <input type="text" id="nombreCliente" class="input_sector input_position" placeholder="Ingresa tu nombre" name="nombreCliente" required>
+              </div>
+               <div class="apellidoModal form-width">
+                <label class="input_label" for="apellido">Apellido:</label>
+                <input type="text" id="apellidoCliente" class="input_sector input_position" placeholder="Ingresa tu apellido" name="apellidoCliente" required>
+              </div>
+              <div class="tarjetaModal form-width">
+                <label class="input_label" for="tarjeta"> Tarjeta de debito/credito:</label>
+                <input type="number" id="tarjetaCliente" class="input_sector input_position" placeholder="EJ: 5427 8217 0217 9192" name="tarjetaCliente" required>
+              </div>
+              <div class="cvvModal form-width">
+                <label class="input_label cvvt" for="CVV" >CVV:</label>
+                <input type="number" id="cvv" class="input_sector" class="input_sector input_position" placeholder="EJ: 512" name="cvvCliente" required>
+              </div>
+              <div class="vencimientoModal" form-width">
+                <label class="input_label" for="fecha">Fecha de vencimiento:</label>
+                <div>
+                    <select type="number" id="mes" class="input_sector mespad" placeholder="2" required>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
+                      <option value="11">11</option>
+                      <option value="12">12</option>
+                    <input type="number" id="anio" class="input_sector aniowidth" placeholder="25" required>
+                </div>
+              </div>
+              <div class="direccionCliente form-width">
+                <div class="direcCliente">
+                  <label class="input_label" for="direccion">Direccion:</label>
+                  <input type="text" id="direccionCliente" class="input_sector input_position" placeholder="Av. Siempreviva 742" required>
+                </div>
+                <div class="CPCliente">
+                  <label class="input_label" for="direccion">Codigo postal:</label>
+                  <input type="text" id="cpCliente" class="input_sector input_position" placeholder="2314" required>
+                </div>
+                <div class="localidadCliente">
+                  <label class="input_label" for="direccion">Localidad:</label>
+                  <input type="text" id="localidadCliente" class="input_sector input_position" placeholder="La Matanza" required>
+                </div>
+              </div>
+              <button id ="finalizar" class="btn btn-success btnposition">
+                          <span>Pagar</span>
+              </button>
+          </form>
+              </div>
+          </div>
+        </div>
+    </div>
+    `;
+
+    const closeModal2 = () => {
+        finalizarModal.innerHTML = "";
+        document.body.style.overflow = 'auto';
+    };
+
+    document.getElementById('finalizar').addEventListener('click', completarPedido);
+
+    function completarPedido(event) {
+        event.preventDefault();
+        const form = document.getElementById('finalizarPagoModal');
+        if (form.checkValidity()) {
+            setTimeout(closeModal2, 0);
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'COMPLETADO',
+                text: 'El pedido llegara a la brevedad',
+                showConfirmButton: false,
+                timer: 1500
+              })
+            resetCarrito();
+        } else {
+            form.reportValidity();
+        }
+    }
+
+    function resetCarrito(){
+        RCarro = [];
+        localStorage.removeItem('saveStateCarro');
+        document.getElementById('tablebody').innerHTML = "";
+        document.getElementById('total').innerHTML = "Total a pagar $:"
+    }
+
+    const cerrarModalPago = document.querySelector("#close-modal2");
+    cerrarModalPago.addEventListener("click", closeModal2);
+    }
+    
+});
+
+document.getElementById('vaciarCompra').addEventListener('click', vaciarCarrito);
+
+function vaciarCarrito(){
+    RCarro = [];
+    localStorage.removeItem('saveStateCarro');
+    document.getElementById('tablebody').innerHTML = "";
+    document.getElementById('total').innerHTML = "Total a pagar $:"
+}
+
--- js/main.js
+++ js/main.js
@@ -9,7 +9,12 @@ const novedades = [{id: 1,
                     editorial: 'IVREA', 
                     demografia: 'SHONEN', 
                     precio: 1800,
-                    enlace: 'SDBH.DDRM01' },
+                    carac:`• Serie de 3 tomos.<br>
+                           • Tomo de aproximadamente 200 páginas.<br>
+                           • Formato tankoubon con sobrecubierta.`,
+                    info: `Tras haber quedado atrapado en un espacio vacío del espaciotiempo, Trunks se ve obligado a convertirse en un Guardián del Tiempo y a sumarse a la eterna lucha contra los Destructores de la historia.<br>
+                    La trama se desarrolla en una realidad alternativa del Dragon World y nos presenta literalmente un sinfín de personajes de diferentes universos y líneas temporales (canónicos y no tanto) reunidos para arrojar toneladas de fan service para alegría de todos.`
+                    },
                    {id: 2, 
                     manga: 'ONE PIECE',
                     portada: 'img/portada/op-37.webp', 
@@ -17,7 +22,13 @@ const novedades = [{id: 1,
                     editorial: 'IVREA', 
                     demografia: 'SHONEN', 
                     precio: 1800,
-                    enlace: 'OP.37'},
+                    carac: `• Tomo de aproximadamente 200 páginas.<br>
+                            • Formato tankoubon con sobrecubierta.<br>
+                            • Incluye desplegable a color.`,
+                    info: `One Piece relata las aventuras de Monkey D. Luffy, un joven que, inspirado en Shanks, un pirata que le salvó la vida, desea convertirse en el Rey de los Piratas y encontrar el tesoro conocido como One Piece, que pertenecía a Gol D. Roger, quien antes de ser ejecutado, hace 24 años, contó al mundo sobre la existencia de su tesoro.<br>
+                    Esto dio inicio a lo que se conoce como "la Gran era de los piratas": innumerables piratas se dispusieron a buscar ese tesoro, causando grandes problemas al Gobierno Mundial.<br>
+                    Luffy iniciará sus aventuras, reclutando amigos y nakamas que lo ayuden a cumplir su sueño.`
+                    },
                    {id: 3, 
                     manga: 'KAIJU N8',
                     portada: 'img/portada/kaiju8-3.webp',  
@@ -25,7 +36,11 @@ const novedades = [{id: 1,
                     editorial: 'IVREA', 
                     demografia: 'SHONEN', 
                     precio: 1800,
-                    enlace: 'KN8.03'},
+                    carac:`• Tomo de aproximadamente 200 páginas.<br>
+                           • Formato tankoubon con sobrecubierta.<br>
+                           • Incluye páginas a color.`,
+                    info: `¡Un hombre, descontento con el trabajo que ha tenido que hacer en la vida, se ve envuelto en un acontecimiento inesperado...! ¡Se convierte en un Kaiju, una criatura monstruosa, dandole asi una nueva oportunidad de lograr lo que siempre soño!`
+                    },
                    {id: 4, 
                     manga: 'RANKING OF KINGS',
                     portada: 'img/portada/rankingOfKings-4.webp',  
@@ -33,7 +48,14 @@ const novedades = [{id: 1,
                     editorial: 'IVREA', 
                     demografia: 'SHONEN', 
                     precio: 2000,
-                    enlace: 'ROK.04'},
+                    carac:`• Tomo de aproximadamente 200 páginas.<br>
+                    • Formato B6 con sobrecubierta.<br>                 
+                    • Incluye páginas a color.`,
+                    info: `Bojji es un príncipe sordomudo que habita en un mundo fantástico repleto de criaturas mágicas.<br>
+                    Pese a no destacar en nada, y siendo incapaz siquiera de empuñar una espadita de niño, sueña con algún día convertirse en el rey más grande del mundo, pese a que todos los que lo conocen lo tildan de "completo inútil".<br>
+                    Todo cambia cuando conoce a Kage, una sombra (literalmente) que es uno de los pocos sobrevivientes de un clan de asesinos y que intenta robarle las pocas pertenencias que Bojji llevaba encima. De alguna manera, Kage logra comprender al príncipe y se convierte en su primer amigo verdadero, dando así inicio a la aventura que Bojji tanto anhela.<br>
+                    Mientras tanto, en la oscuridad, un poder amenaza con devastar al reino.`
+                    },
                    {id: 5, 
                     manga: 'WE NEVER LEARN',
                     portada: 'img/portada/weNeverLearn-18.webp',  
@@ -41,7 +63,11 @@ const novedades = [{id: 1,
                     editorial: 'IVREA', 
                     demografia: 'SHONEN', 
                     precio: 1800,
-                    enlace: 'WNL.18'},
+                    carac:`• Tomo de aproximadamente 200 páginas.<br>
+                    • Formato B6 con sobrecubierta.<br>                  
+                    • Incluye páginas a color.`,
+                    info: `La historia se centra en Yuiga Nariyuki, un estudiante de clase social pobre que nacio con la inteligencia de una persona ordinaria, sin destacar en nada. Se esfuerza al maximo por ser el mejor de la clase y obtener una beca en la universidad, donde aspira graduarse y darle a su familia una mejor vida. Por cosas del destino se ve envuelto en una situacion de la que no puede escapar si quiere obtener su beca universitaria: Ser el tutor de... ¡¿Unas genios?!`
+                    },
                    {id: 6, 
                     manga: 'BLACK PARADOX',
                     portada: 'img/portada/blackParadox.webp',  
@@ -49,7 +75,11 @@ const novedades = [{id: 1,
                     editorial: 'IVREA', 
                     demografia: 'SEINEN', 
                     precio: 3900,
-                    enlace: 'BP'},
+                    carac:`• Tomo ÚNICO de aproximadamente 240 páginas.<br>
+                    • Formato A5 con sobrecubierta.<br>
+                    ​• Incluye historia extra íntegramente a color.`,
+                    info: `La historia narra las vivencias de cuatro personas que se conocen online y deciden hacer un viaje con un objetivo en común: ponerle fin a sus vidas de forma rápida e indolora. Sin embargo, sus planes se complicarán cuando el primer intento... no les salga bien y al segundo terminen descubriendo un pasadizo a otro mundo, uno con unas piedras preciosas invaluables y poderosas con el potencial de cambiarles su destino... Pero nadie entiende bien cuál es su verdadera naturaleza y mientras más puertas a otros mundos se abren, los cuatro se ven forzados a replantearse su vida y su muerte.`
+                    },
                    {id: 7, 
                     manga: 'BLACK CLOVER',
                     portada: 'img/portada/blackClover-23.webp',  
@@ -57,7 +87,10 @@ const novedades = [{id: 1,
                     editorial: 'IVREA', 
                     demografia: 'SHONEN', 
                     precio: 1800,
-                    enlace: 'BC.23'},
+                    carac:`• Tomo de aproximadamente 200 páginas.<br>
+                    • Formato tankoubon con sobrecubierta.`,
+                    info: `En una iglesia ubicada en una aldea remota del Reino Trebol, viven Aste y Yuno, quienes han sido amigos desde que fueron abandonados de bebes en ese lugar. Yuno posee un enorme poder magico y es muy habil para lanzar hechizos; en contraste, Aste nunca ha podido usar magia. En un mundo donde eres juzgado segun tu procedencia y poder magico. Asta busca demostrar que cualquiera puede convertirse en alguien asombroso. Esta es su resolucion: convertirse en el Rey Mago. Inspirado por el, Yuno se propone lo mismo.`
+                    },
                    {id: 8, 
                     manga: 'SAINT SEIYA: NEXT DIMENSION (NUEVA EDICION)',
                     portada: 'img/portada/saintSeiya-12.webp',  
@@ -65,7 +98,12 @@ const novedades = [{id: 1,
                     editorial: 'IVREA', 
                     demografia: 'SHONEN', 
                     precio: 3500,
-                    enlace: 'SSND.12'},
+                    carac:`• Tomo de aproximadamente 200 páginas.<br>
+                    • Formato B6, sobrecubierta con laca sectorizada.<br>  
+                    ​• Integramente a color.`,
+                    info: `Ambientado más de dos siglos antes de los sucesos de Saint Seiya, la serie original, este manga relata la sangrienta Guerra Sagrada que se llevará la vida de la gran mayoría de los Caballeros.<br>
+                    Tenma, el recién nombrado caballero de Pegaso y Alone, la reencarnación de Hades, son dos amigos de toda la vida cuyos destinos los obligan a enfrentarse entre sí en una batalla épica de la que depende el futuro de la humanidad.`
+                    },
                    {id: 9, 
                     manga: 'MUSHOKU TENSEI',
                     portada: 'img/portada/mushokuTensei-5.jpeg',  
@@ -73,7 +111,10 @@ const novedades = [{id: 1,
                     editorial: 'PANINI MANGA', 
                     demografia: 'SEINEN', 
                     precio: 2200,
-                    enlace: 'MT.05'},
+                    carac:`• Tomo de aproximadamente 172 páginas.<br>
+                           • Formato tankoubon con sobrecubierta.`,
+                    info:`¿Qué pasa cuando un desempleado de más de 40 años pierde su vida salvando a otros? Pues que reencarna en un mundo de espadas y hechicería, ¡claro! Rudeus conserva todos los recuerdos de su vida anterior y es por eso que en esta vida se ha propuesto esforzarse para vivir su vida al máximo. Aprenderá de hechicería y de técnicas de la espada para sentir la satisfacción que no pudo obtener antes de morir.`
+                    },
                    {id: 10, 
                     manga: 'WIND BREAKER',
                     portada: 'img/portada/windBreaker-2.jpeg',  
@@ -81,7 +122,10 @@ const novedades = [{id: 1,
                     editorial: 'PANINI MANGA', 
                     demografia: 'SHONEN', 
                     precio: 2100,
-                    enlace: 'WB.02'},
+                    carac:`• Tomo de aproximadamente 192 páginas.<br>
+                           • Formato tankoubon con sobrecubierta.`,
+                    info: `Haruka Sakura es un estudiante de nuevo ingreso en la preparatoria Furin. Él decidió ir a esta escuela con un objetivo: derrocar al jefe de la pandilla de la Furin. Está por verse si lo logrará o no.`
+                    },
                    {id: 11, 
                     manga: 'MUSHISHI',
                     portada: 'img/portada/mushishi-1.jpeg',  
@@ -89,7 +133,10 @@ const novedades = [{id: 1,
                     editorial: 'PANINI MANGA', 
                     demografia: 'SEINEN', 
                     precio: 2800,
-                    enlace: 'MSS.01'},
+                    carac:`• Tomo de aproximadamente 240 páginas.<br>
+                    • Formato tankoubon con sobrecubierta.`,
+                    info: `Los Mushi son unas formas de vida muy extrañas, una especie de espíritus de la naturaleza capaces de realizar proezas mágicas no siempre benficiosas para los humanos. Sólo Ginko, el maestro de los bichos, tiene el poder de enfrentarse a ellos y detener la plaga que asola el mundo.`
+                    },
                    {id: 12, 
                     manga: 'PERFECT WORLD',
                     portada: 'img/portada/perfectWorld-5.webp',  
@@ -97,7 +144,10 @@ const novedades = [{id: 1,
                     editorial: 'PANINI MANGA', 
                     demografia: 'JOSEI', 
                     precio: 2100,
-                    enlace: 'PW.05'}
+                    carac:`• Tomo de aproximadamente 172 páginas.<br>
+                    • Formato tankoubon con sobrecubierta.`,
+                    info: `¿Cuáles son las barreras que plantea una relación con una persona con discapacidad? Tsugumi Kawana, de 26 años, consiguió un trabajo en una empresa de diseño de interiores. Una noche, en una cena de trabajo con clientes de un estudio de arquitectura, se reencuentra con el que fue su primer amor de la preparatoria, Itsuki Ayukawa, y sus sentimientos hacia él empiezan a revivir. Al descubrir que ahora va en silla de ruedas, piensa que su amor será imposible.`
+                    }
                    ];
 
 const productos = [{id: 1, 
@@ -106,96 +156,84 @@ const productos = [{id: 1,
                     num: 01, 
                     editorial: 'IVREA', 
                     demografia: 'SHONEN', 
-                    precio: 1800,
-                    enlace: 'SDBH.DDRM01' },
+                    precio: 1800,},
                    {id: 2, 
                     manga: 'ONE PIECE',
                     portada: 'img/portada/op-37.webp', 
                     num: 37, 
                     editorial: 'IVREA', 
                     demografia: 'SHONEN', 
-                    precio: 1800,
-                    enlace: 'OP.37'},
+                    precio: 1800,},
                    {id: 3, 
                     manga: 'KAIJU N8',
                     portada: 'img/portada/kaiju8-3.webp',  
                     num: 03, 
                     editorial: 'IVREA', 
                     demografia: 'SHONEN', 
-                    precio: 1800,
-                    enlace: 'KN8.03'},
+                    precio: 1800,},
                    {id: 4, 
                     manga: 'RANKING OF KINGS',
                     portada: 'img/portada/rankingOfKings-4.webp',  
                     num: 04, 
                     editorial: 'IVREA', 
                     demografia: 'SHONEN', 
-                    precio: 2000,
-                    enlace: 'ROK.04'},
+                    precio: 2000,},
                    {id: 5, 
                     manga: 'WE NEVER LEARN',
                     portada: 'img/portada/weNeverLearn-18.webp',  
                     num: 18, 
                     editorial: 'IVREA', 
                     demografia: 'SHONEN', 
-                    precio: 1800,
-                    enlace: 'WNL.18'},
+                    precio: 1800,},
                    {id: 6, 
                     manga: 'BLACK PARADOX',
                     portada: 'img/portada/blackParadox.webp',  
                     num: " ", 
                     editorial: 'IVREA', 
                     demografia: 'SEINEN', 
-                    precio: 3900,
-                    enlace: 'BP'},
+                    precio: 3900,},
                    {id: 7, 
                     manga: 'BLACK CLOVER',
                     portada: 'img/portada/blackClover-23.webp',  
                     num: 23, 
                     editorial: 'IVREA', 
                     demografia: 'SHONEN', 
-                    precio: 1800,
-                    enlace: 'BC.23'},
+                    precio: 1800,},
                    {id: 8, 
                     manga: 'SAINT SEIYA: NEXT DIMENSION (NUEVA EDICION)',
                     portada: 'img/portada/saintSeiya-12.webp',  
                     num: 12, 
                     editorial: 'IVREA', 
                     demografia: 'SHONEN', 
-                    precio: 3500,
-                    enlace: 'SSND.12'},
+                    precio: 3500,},
                    {id: 9, 
                     manga: 'MUSHOKU TENSEI',
                     portada: 'img/portada/mushokuTensei-5.jpeg',  
                     num: 05, 
                     editorial: 'PANINI MANGA', 
                     demografia: 'SEINEN', 
-                    precio: 2200,
-                    enlace: 'MT.05'},
+                    precio: 2200,},
                    {id: 10, 
                     manga: 'WIND BREAKER',
                     portada: 'img/portada/windBreaker-2.jpeg',  
                     num: 02, 
                     editorial: 'PANINI MANGA', 
                     demografia: 'SHONEN', 
-                    precio: 2100,
-                    enlace: 'WB.02'},
+                    precio: 2100,},
                    {id: 11, 
                     manga: 'MUSHISHI',
                     portada: 'img/portada/mushishi-1.jpeg',  
                     num: 01, 
                     editorial: 'PANINI MANGA', 
                     demografia: 'SEINEN', 
-                    precio: 2800,
-                    enlace: 'MSS.01'},
+                    precio: 2800,},
                    {id: 12, 
                     manga: 'PERFECT WORLD',
                     portada: 'img/portada/perfectWorld-5.webp',  
                     num: 05, 
                     editorial: 'PANINI MANGA', 
                     demografia: 'JOSEI', 
-                    precio: 2100,
-                    enlace: 'PW.05'}
+                    precio: 2100,}
                    ];
 
 const carrito = [];
@@ -260,9 +298,9 @@ function mostrarModalPreview(e) {
                 </div>
                 <button id="car${encontrado.id}" type="button" class="btn btn-outline-success carrito"> 🛒 AGREGAR AL CARRITO</button>
                 <p class="miniinfo">
-                • Tomo de aproximadamente 200 páginas.<br>• Incluye desplegable a color.<br>• Formato tankoubon con sobrecubierta.
+                ${encontrado.carac}
                 </p>
-                <p class="infoinfo">One Piece relata las aventuras de Monkey D. Luffy, un joven que, inspirado en Shanks, un pirata que le salvó la vida, desea convertirse en el Rey de los Piratas y encontrar el tesoro conocido como One Piece, que pertenecía a Gol D. Roger, quien antes de ser ejecutado, hace 24 años, contó al mundo sobre la existencia de su tesoro.</p>
+                <p class="infoinfo">${encontrado.info}</p>
             </div>
         </div>
     </div>
@@ -282,11 +320,20 @@ function mostrarModalPreview(e) {
     const cerrarModalButton = document.querySelector("#close-modal");
     cerrarModalButton.addEventListener("click", closeModal);
 
+    const cerrarModal2Button = document.querySelector(`#car${encontrado.id}`);
+    cerrarModal2Button.addEventListener("click", closeModal);
+
     // Agregado de objetos al carrito
     const agregarAlCarritoButton = document.querySelector(`#car${encontrado.id}`);
     agregarAlCarritoButton.addEventListener("click", () => {
     carrito.push(encontrado);
     console.table(carrito);
+    Swal.fire({
+        icon: 'success',
+        title: '¡Agregado al carrito!',
+        text: `${encontrado.manga} ${encontrado.num} se ha agregado al carrito`,
+        timer: 3000
+      })
     document.getElementById('carrito').style.display = 'block';
     });
 
